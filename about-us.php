7c3f41f460a2c40a660421d4e092b3e401f9e222
oyldrr <oyldrr@gmail.com>
2023-06-13 13:35:07 +0300

    about us fixed

--- about-us.php
+++ about-us.php
@@ -106,6 +106,24 @@ include_once "header.php";
 
     <?php include_once "footer.php"; ?>
 </body>
-<script src="js/script.js"></script>
+<script>
+    /* About us Scroll Timeline Effect */
+    document.addEventListener('scroll', timeline);
+
+    function timeline() {
+        var threshold_position = window.scrollY + window.innerHeight * 2 / 3;
+        //compare scrolltop with scrolltop on each timeline event
+        var timeline_events = document.querySelectorAll('.timeline li');
+        for (i in timeline_events) {
+            if (timeline_events[i].offsetTop < threshold_position) {
+                timeline_events[i].classList.add('visible');
+            } else {
+                timeline_events[i].classList.remove('visible');
+            }
+        }
+    }
+    timeline();
+    /* */
+</script>
 
 </html>
--- js/script.js
+++ js/script.js
@@ -109,9 +109,9 @@ function toggleOptions() {
   }
   else {
     if (trigger.innerHTML == "Finish Early") {
-      hoursElement.innerText = 00;
-      minutesElement.innerText = 00;
-      secondsElement.innerText = 00;
+      hoursElement.innerText = "00";
+      minutesElement.innerText = "00";
+      secondsElement.innerText = "00";
       message.innerText = "Failed! :(";
       trigger.innerHTML == "Start Now!";
       isOpened = "false";
@@ -127,22 +127,4 @@ function toggleOptions() {
 
 }
 
-/* */
-
-/* About us Scroll Timeline Effect */
-document.addEventListener('scroll', timeline);
-
-function timeline() {
-  var threshold_position = window.scrollY + window.innerHeight * 2 / 3;
-  //compare scrolltop with scrolltop on each timeline event
-  var timeline_events = document.querySelectorAll('.timeline li');
-  for (i in timeline_events) {
-    if (timeline_events[i].offsetTop < threshold_position) {
-      timeline_events[i].classList.add('visible');
-    } else {
-      timeline_events[i].classList.remove('visible');
-    }
-  }
-}
-timeline();
 /* */
